cc61e7793ccf6b1e1909fb1fa4c79cd0b1cbbd53
arnest00 <53844657+arnest00@users.noreply.github.com>
2020-09-28 19:14:47 -0400

    Update from working portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
+const menuBtns = document.querySelectorAll('.menu-button');
+const logoBtn = document.querySelector('#logo');
+const navLinksContainer = document.querySelector('#nav-links-container');
 const navLinks = document.querySelectorAll('.nav-link');
+
+menuBtns.forEach(button => {
+  button.addEventListener('click', e => {
+    navLinksContainer.classList.toggle('hidden');
+    toggleMenuBtns();
+  });
+});
+
+logoBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  });
+});;
 
 navLinks.forEach(link => {
   link.addEventListener('click', function (e) {
@@ -7,5 +25,14 @@ navLinks.forEach(link => {
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
+
+    navLinksContainer.classList.toggle('hidden');
+    toggleMenuBtns();
   });
 });
+
+const toggleMenuBtns = () => {
+  for (let btn = 0; btn <= menuBtns.length; btn++) {
+    menuBtns[btn].classList.toggle('hidden');
+  };
+};
