f4322330db76c2e9af68ea12533933197d6b5280
Osmond <osmondarnesto@gmail.com>
2021-06-29 14:33:52 -0700

    Restructure html, update styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const menuBtns = document.querySelectorAll('.menu-button');
-const logoBtn = document.querySelector('#logo');
+const scrollToTopBtn = document.querySelector('#scroll-to-top');
+const headerSection = document.querySelector('#header');
 const navLinksContainer = document.querySelector('#nav-links-container');
 const navLinks = document.querySelectorAll('.to-section');
 
@@ -9,14 +10,6 @@ menuBtns.forEach(button => {
     toggleMenuBtns();
   });
 });
-
-logoBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  document.querySelector(this.getAttribute('href')).scrollIntoView({
-    behavior: 'smooth'
-  });
-});;
 
 navLinks.forEach(link => {
   link.addEventListener('click', function (e) {
@@ -33,6 +26,20 @@ navLinks.forEach(link => {
 
 const toggleMenuBtns = () => {
   for (let btn = 0; btn <= menuBtns.length; btn++) {
-    menuBtns[btn].classList.toggle('hidden');
+    menuBtns[btn].classList.toggle('transparent');
   };
+};
+
+window.onscroll = () => {
+  if (window.scrollX === 0)
+    scrollToTopBtn.classList.add('transparent');
+
+  if (window.scrollY > headerSection.offsetTop)
+    scrollToTopBtn.classList.remove('transparent');
+};
+
+scrollToTopBtn.onclick = () => {
+  window.scrollTo(0, 0);
+
+  scrollToTopBtn.classList.add('transparent');
 };
