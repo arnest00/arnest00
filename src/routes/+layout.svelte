13a3b12a4fe96bf993fac7e48177846681ccb33a
Osmond Arnesto <osmond@heysparkbox.com>
2024-03-23 12:29:47 -0700

    feat: Import content and styling from design placeholder

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import './styles.scss'
+</script>
+
+<div>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style lang="scss">
+    main {
+      width: 75vw;
+      height: 100vh;
+      margin: 0 auto;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-transform: lowercase;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Osmond Arnesto</title>
+</svelte:head>
+
+<h1>Closed for renovations</h1>
+
+<p>Need to contact me in the meantime?</p>
+
+<ul>
+  <li>
+    <a
+      href="https://www.linkedin.com/in/osmondarnesto/"
+      rel="noopener noreferrer">
+      Visit my LinkedIn
+    </a>
+  </li>
+  <li>
+    <a 
+      href="https://github.com/arnest00"
+      rel="noopener noreferrer">
+      Check out my GitHub
+    </a>
+  </li>
+</ul>
+
+<style lang="scss">
+  a {
+    text-decoration: none;
+    color: var(--link-color);
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &:visited { 
+      color: var(--link-visited-color);
+    }
+  }
+</style>
